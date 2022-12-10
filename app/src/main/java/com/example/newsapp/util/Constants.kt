49da8ae28087998647518c74d44c2d66fc0d9d5f
muhamedsultan21 <sultanmedo49@gmail.com>
2022-12-10 19:08:28 +0200

    NewsApp

--- app/src/main/java/com/example/newsapp/util/Constants.kt
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -2,7 +2,7 @@ package com.example.newsapp.util
 
 class Constants {
     companion object {
-        const val API_KEY = "6953a7421bb749c589e687746c85eee8"
+        const val API_KEY = "f4c5fbeabfe34036ad2de54fd8b4619a"
         const val BASE_URL = "https://newsapi.org"
         const val SEARCH_NEWS_TIME_DELAY = 500L
         const val QUERY_PAGE_SIZE = 20
